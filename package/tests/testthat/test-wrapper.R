ce64ddb1ec11974b19fd6b861597eabd21f543da
Robrecht Cannoodt <rcannood@gmail.com>
2020-02-13 10:38:08 +0100

    add test

--- package/tests/testthat/test-wrapper.R
+++ package/tests/testthat/test-wrapper.R
@@ -1,7 +1,7 @@
 test_that("ti_slingshot works", {
   dataset <- source(system.file("example.sh", package = "tislingshot"))$value
 
-  model <- dynwrap::infer_trajectory(dataset, tislingshot::ti_slingshot(), verbose = T)
+  model <- dynwrap::infer_trajectory(dataset, tislingshot::ti_slingshot(), verbose = TRUE)
 
   expect_is(model, "dynwrap::with_trajectory")
 })
